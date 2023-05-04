2723bb9a964dbc34ef7c9dba2d69e1e5ccfc7dbc
ryohpops <ryohpops@users.noreply.github.com>
2023-05-05 03:12:00 +0900

    Update config

--- next.config.js
+++ next.config.js
@@ -1,24 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false
-      }
-    }
-    return config
-  },
-  async rewrites() {
-    return {
-      fallback: [
-        {
-          source: "/api/:path*",
-          destination: "http://backend:5000/api/:path*"
-        }
-      ]
-    }
-  }
+  output: 'export'
 }
 
 module.exports = nextConfig
